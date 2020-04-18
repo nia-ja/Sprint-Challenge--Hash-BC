bac167d9727f9a83443fab9b28f062f8cd5088aa
Kasia Bondarava <kasia.bondarava@gmail.com>
2020-04-17 20:13:33 -0400

    Added solution for blockchain part

--- blockchain/miner.py
+++ blockchain/miner.py
@@ -26,6 +26,16 @@ def proof_of_work(last_proof):
     proof = 0
     #  TODO: Your code here
 
+    # setting proof to last_proof times a random int between 0 and 100 so to not start at 0 to make mining coins happen faster.
+    # proof = last_proof*randint(0, 100)
+
+    new_last_proof = f'{last_proof}'.encode()
+
+    last_hash = hashlib.sha256(new_last_proof).hexdigest()
+
+    while valid_proof(last_hash, proof) is False:
+        proof += 1
+
     print("Proof found: " + str(proof) + " in " + str(timer() - start))
     return proof
 
@@ -40,7 +50,11 @@ def valid_proof(last_hash, proof):
     """
 
     # TODO: Your code here!
-    pass
+    guess = f'{proof}'.encode()
+    guess_hash = hashlib.sha256(guess).hexdigest()
+
+    # last 5 of last hash equal the first 5 of the guess
+    return last_hash[-5:] == guess_hash[:5]
 
 
 if __name__ == '__main__':
